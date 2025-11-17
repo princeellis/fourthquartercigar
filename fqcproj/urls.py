287a6f99d032fb32c443160174c33b40991acc82
Elisha McDuffie <elisha.mcduffie@gmail.com>
2025-11-16 16:41:56 -0800

    Fix media file serving in production with explicit serve view

--- fqcproj/urls.py
+++ fqcproj/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 from base import views
 
 urlpatterns = [
@@ -33,4 +34,7 @@ if settings.DEBUG:
     urlpatterns += staticfiles_urlpatterns()
 
 # Serve media files in both development and production
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Use re_path with serve view for production compatibility
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
